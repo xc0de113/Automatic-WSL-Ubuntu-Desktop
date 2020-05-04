b3ce9c630c5a44704ecbbb3e4918c69e22f9970d
xc0de113 <62719107+xc0de113@users.noreply.github.com>
2020-05-04 08:31:48 -0700

    Created ubuntudesktop-v2.sh

---
+++ ubuntudesktop-v2.sh
@@ -0,0 +1,12 @@
+mkdir /mnt/c/users/public
+mkdir /mnt/c/users/public/desktop
+cd /mnt/c/users/public/desktop
+wget https://managedway.dl.sourceforge.net/project/vcxsrv/vcxsrv/1.20.8.1/vcxsrv-64.1.20.8.1.installer.exe -O vcxsrv.exe
+wget https://raw.githubusercontent.com/xc0de113/Automatic-WSL-Ubuntu-Desktop/master/install-v2.sh -O install.sh
+echo "======================================================================================================="
+echo ""
+echo "Run this application. It is in C:\Users\Public\Desktop and is called vcxsrv.exe"
+echo "Then run install.sh. It is in the same directory."
+echo "You can use the command 'cd /mnt/c/users/public/desktop && sudo ./install.sh'"
+echo ""
+echo "======================================================================================================="
