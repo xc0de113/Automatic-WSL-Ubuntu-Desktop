43d77ae15c11a9e4759b438503d01b2b1dd4a8f0
xc0de113 <62719107+xc0de113@users.noreply.github.com>
2020-05-04 09:03:39 -0700

    Created v2 startsession

---
+++ startsession-v2.sh
@@ -0,0 +1,6 @@
+echo "======================================================================="
+echo ""
+echo "STARTING XFCE4. PRESS CTRL+C TO QUIT, AND LOGOUT IN THE GUI TO QUIT."
+echo ""
+echo "======================================================================="
+startxfce4
