7a692a86e8c3f9a8ee3aee8d440d05d3c1c16fb3
xc0de113 <62719107+xc0de113@users.noreply.github.com>
2020-05-03 17:38:38 -0700

    Edited ubuntudesktop.sh

--- ubuntudesktop-v1.sh
+++ ubuntudesktop-v1.sh
@@ -2,7 +2,7 @@ mkdir /mnt/c/users/public
 mkdir /mnt/c/users/public/desktop
 cd /mnt/c/users/public/desktop
 wget https://managedway.dl.sourceforge.net/project/vcxsrv/vcxsrv/1.20.8.1/vcxsrv-64.1.20.8.1.installer.exe
-wget https://raw.githubusercontent.com/xc0de113/Automatic-WSL-Ubuntu-Desktop/master/install.sh
+wget https://raw.githubusercontent.com/xc0de113/Automatic-WSL-Ubuntu-Desktop/master/install-v1.sh
 echo "======================================================================================================="
 echo ""
 echo "Run this application. It is in C:\Users\Public\Desktop and is called vcxsrv-64.1.20.8.1.installer.exe"
